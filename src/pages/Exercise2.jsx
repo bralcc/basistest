433067df609f1d398f4d53eff7fd23b193db2bc4
bralcc <cedric.bral@vdabcampus.be>
2025-05-24 12:29:02 +0200

    Moving script to seperate file for exercise 2 maintaining readability

--- src/pages/Exercise2.jsx
+++ src/pages/Exercise2.jsx
@@ -1,52 +1,5 @@
-const directions = ["rightUp", "rightDown", "leftUp", "leftDown"];
-const colors = ["black", "white"];
+import React from "react";
 
-function chooseColorsRandomly() {
-  let chosenColors = [];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  chosenColors.push(randomColor);
-  // Push the opposite color
-  chosenColors.push(randomColor === "white" ? "black" : "white");
-  return chosenColors;
-}
-
-function chooseDirectionsRandomly() {
-  let chosenDirections = [];
-  chosenDirections.push(
-    directions[Math.floor(Math.random() * directions.length)]
-  );
-  chosenDirections.push(
-    directions[Math.floor(Math.random() * directions.length)]
-  );
-  return chosenDirections;
-}
-
-function constructCorrectAnswer() {
-  return {
-    directions: chooseDirectionsRandomly(),
-    colors: chooseColorsRandomly(),
-  };
-}
-
-const correctAnswer = constructCorrectAnswer();
-
-function constructRandomAnswers(max, correctAnswer) {
-  const randomAnswers = [];
-  while (randomAnswers.length < max) {
-    let randomAnswer = {
-      directions: chooseDirectionsRandomly(),
-      colors: chooseColorsRandomly(),
-    };
-    // Ensure random answer is not the same as the correct answer
-    const isSame =
-      JSON.stringify(randomAnswer.directions) ===
-        JSON.stringify(correctAnswer.directions) &&
-      JSON.stringify(randomAnswer.colors) ===
-        JSON.stringify(correctAnswer.colors);
-    if (!isSame) {
-      randomAnswers.push(randomAnswer);
-    }
-    // If same, try again
-  }
-  return randomAnswers;
+export default function Exercise2() {
+  return <div>Exercise2</div>;
 }

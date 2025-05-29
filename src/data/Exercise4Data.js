f3537b4622edd46066896f459c31b134536e7bd9
bralcc <cedric.bral@gmail.com>
2025-05-29 16:12:31 +0200

    added data and script for constructing the question along with statement and answer

---
+++ src/data/Exercise4Data.js
@@ -0,0 +1,44 @@
+const Exercise4Data = [
+  {
+    id: 1,
+    words: ["Veerboot", "Tram", "Truck", "Helikopter"],
+    comparisons: [
+      ["<", "trager dan"],
+      [">", "vlugger dan"],
+    ],
+  },
+  {
+    id: 2,
+    words: ["Meer", "Berg", "Bos", "Rivier"],
+    comparisons: [
+      ["<", "dichterbij dan"],
+      [">", "verder dan"],
+    ],
+  },
+  {
+    id: 3,
+    words: ["Patrouille", "Regiment", "Compagnie", "Eskader"],
+    comparisons: [
+      ["<", "links van"],
+      [">", "rechts van"],
+    ],
+  },
+  {
+    id: 4,
+    words: ["Helm", "Bajonet", "Geweer", "Mes"],
+    comparisons: [
+      ["<", "lichter dan"],
+      [">", "zwaarder dan"],
+    ],
+  },
+  {
+    id: 5,
+    words: ["Politieman", "Pompier", "Piloot", "Soldaat"],
+    comparisons: [
+      ["<", "kleiner dan"],
+      [">", "groter dan"],
+    ],
+  },
+];
+
+export default Exercise4Data;
---
+++ src/utils/Exercise4Utils.js
@@ -0,0 +1,70 @@
+import Exercise4Data from "../data/Exercise4Data.js";
+
+const data = Exercise4Data;
+
+function generateRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i >= 1; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function pickRandomObject(data) {
+  return data[generateRandomNumber(data.length)];
+}
+
+function pickUniqueRandomWords(object) {
+  return shuffleArray([...object.words]).slice(0, 3);
+}
+
+function questionWordMap(object, askLargest) {
+  switch (object.id) {
+    case 1:
+      return askLargest ? "vlugst" : "traagst";
+    case 2:
+      return askLargest ? "verst" : "dichtst";
+    case 3:
+      return askLargest ? "verst naar rechts" : "verst naar links";
+    case 4:
+      return askLargest ? "zwaarst" : "lichtst";
+    case 5:
+      return askLargest ? "grootst" : "kleinst";
+  }
+}
+
+function constructRandomComparisonExercise(data) {
+  const object = pickRandomObject(data);
+  const [a, b, c] = pickUniqueRandomWords(object);
+  let randomIndex = generateRandomNumber(1);
+
+  const comp1Text = object.comparisons[randomIndex][1];
+  randomIndex = generateRandomNumber(1);
+
+  const comp2Text = object.comparisons[randomIndex][1];
+  const askLargest = Math.random() < 0.5;
+
+  let statements = shuffleArray([
+    [`${a} ${comp1Text} ${b}`],
+    [`${b} ${comp2Text} ${c}`],
+  ]);
+
+  let answer;
+  if (askLargest) {
+    answer = c;
+  } else {
+    answer = a;
+  }
+  const question = `Wat is ${questionWordMap(object, askLargest)}?`;
+  return { statements, question, answer };
+}
+
+// Example usage:
+const exercise = constructRandomComparisonExercise(data);
+console.log(exercise.statements.join("\n"));
+console.log(exercise.question);
+console.log("Answer:", exercise.answer);
